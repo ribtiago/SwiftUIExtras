cc3b21be1a2681e18634b45dbe8d2728bfbf39e0
Tiago Ribeiro <tiago.ribeiro@necam.com>
2023-03-07 18:32:30 +0000

    added Orientation property wrapper

---
+++ Sources/SwiftUIExtras/Property Wrappers/Orientation.swift
@@ -0,0 +1,32 @@
+//
+//  Orientation.swift
+//  
+//
+//  Created by Tiago Ribeiro on 07/03/2023.
+//
+
+import SwiftUI
+import Combine
+
+@propertyWrapper public struct Orientation: DynamicProperty {
+    
+    @StateObject private var coordinator = OrientationCoordinator()
+    
+    public var wrappedValue: UIDeviceOrientation {
+        return coordinator.orientation
+    }
+    
+    public init() { }
+    
+    class OrientationCoordinator: ObservableObject {
+        @Published public var orientation =  UIDevice.current.orientation
+        
+        init() {
+            NotificationCenter.default
+                .publisher(for: UIDevice.orientationDidChangeNotification)
+                .map { _ in UIDevice.current.orientation }
+                .assign(to: &self.$orientation)
+        }
+    }
+}
+
